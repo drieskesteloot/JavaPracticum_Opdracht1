928eaf5353ade5c3518e9bbcce4172db23bf40ce
Sara <Sara@10.25.102.192>
2015-10-05 20:41:45 +0200

    test-package en test-klasse

---
+++ Opdracht_1/src/testing/Test.java
@@ -0,0 +1,13 @@
+package testing;
+
+import static org.junit.Assert.*;
+import datum.Datum;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
